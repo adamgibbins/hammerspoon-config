344ce0f117e118918ea1c5c55d2ea76b8b71d05c
Adam Gibbins <adam@adamgibbins.com>
2015-10-13 23:24:07 +0100

    Revert "Setting the volume to 0 doesnt always work, not sure why, so used muted instead"
    This reverts commit 9afb5b036cbff4c284473982cdd02d150debd0ab.

--- init.lua
+++ init.lua
@@ -35,7 +35,7 @@ end):start()
 hs.caffeinate.watcher.new(function(event)
   -- Mute sounds on suspend, or if shutting down - to stop the startup chime
   if event == hs.caffeinate.watcher.systemWillSleep or event == hs.caffeinate.watcher.systemWillPowerOff then
-    hs.audiodevice.defaultOutputDevice():setMuted(true)
+    hs.audiodevice.defaultOutputDevice():setVolume(0)
   end
 end):start()
 
