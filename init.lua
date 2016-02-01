e5908c504df3aadd5dd22e60e8c79c748c3404c6
Adam Gibbins <adam@adamgibbins.com>
2016-02-01 23:14:24 +0000

    Add 1password

--- init.lua
+++ init.lua
@@ -30,6 +30,7 @@ local laptopLayout = {
   { 'Tweetbot',                 nil,          hs.screen.find(screenInternal), hs.layout.left50,       nil, nil },
   { 'Google Chrome',            nil,          hs.screen.find(screenInternal), hs.layout.right75,      nil, nil },
   { 'FirefoxDeveloperEdition',  nil,          hs.screen.find(screenInternal), hs.layout.right75,      nil, nil },
+  { '1Password 6',              nil,          hs.screen.find(screenInternal), hs.layout.right70,      nil, nil },
   { 'Dash',                     nil,          hs.screen.find(screenInternal), hs.layout.maximized,    nil, nil },
   { 'Skype',                    nil,          hs.screen.find(screenInternal), hs.layout.left25,       nil, nil },
 }
